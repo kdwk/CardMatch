4218ad2fd26cec0117c6bfa1f31a3faf4e6dd57f
kdwk <kdwkleung@proton.me>
2023-11-16 19:01:49 +0800

    Finish javadoc

--- src/BoxBuilder.java
+++ src/BoxBuilder.java
@@ -14,16 +14,28 @@ public class BoxBuilder {
     private JPanel panel = new JPanel();
     
     /**
-     * Construct a new BoxBuilder
+     * Constructs a new BoxBuilder, with a JPanel with BoxLayout with PAGE_AXIS by default
      */
-    public BoxBuilder() {}
+    public BoxBuilder() {
+        this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.PAGE_AXIS));
+    }
     
     /**
-     * Construct a new BoxBuilder and register it in the global Components registry
+     * Construct a new BoxBuilder, with a JPanel with BoxLayout with PAGE_AXIS by default, 
+     * and register it in the global Components registry
      * @param id
      */
-    public BoxBuilder(String id) {Components.reg(id, this.panel);}
+    public BoxBuilder(String id) {
+        this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.PAGE_AXIS));
+        Components.reg(id, this.panel);
+    }
 
+    /**
+     * Registers a Component and adds it, wrapped in a JPanel, into this Box
+     * @param id The ID of the Component, for registration purposes
+     * @param component The Component to be added
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder add(String id, Component component) {
         JPanel componentPanel = new JPanel();
         componentPanel.add(component);
@@ -32,6 +44,11 @@ public class BoxBuilder {
         return this;
     }
 
+    /**
+     * Adds a Component, wrapped in a JPanel, into this Box
+     * @param component The Component to be added
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder add(Component component) {
         JPanel componentPanel = new JPanel();
         componentPanel.add(component);
@@ -39,46 +56,84 @@ public class BoxBuilder {
         return this;
     }
 
+    /**
+     * Sets the layout axis of the BoxLayout used by the JPanel
+     * @param axis BoxLayout.{LINE,PAGE,X,Y}_AXIS
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder layoutAxis(int axis) {
         this.panel.setLayout(new BoxLayout(this.panel, axis));
         return this;
     }
 
+    /**
+     * Sets the x-alignment of the JPanel
+     * @param alignment Component.{BOTTOM,TOP,LEFT,RIGHT,CENTER}_ALIGNMENT
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder alignX(float alignment) {
         this.panel.setAlignmentX(alignment);
         return this;
     }
 
+    /**
+     * Sets the y-alignment of the JPanel
+     * @param alignment Component.{BOTTOM,TOP,LEFT,RIGHT,CENTER}_ALIGNMENT
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder alignY(float alignment) {
         this.panel.setAlignmentY(alignment);
         return this;
     }
 
-    public BoxBuilder doubleBuffered(boolean doubleBuffered) {
-        this.panel.setDoubleBuffered(doubleBuffered);
-        return this;
-    }
-
+    /**
+     * Sets the background colour of the JPanel
+     * @param color The desired Color
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder background(Color color) {
         this.panel.setBackground(color);
         return this;
     }
 
+    /**
+     * Sets the maximum size of the JPanel
+     * @param width Maximum width
+     * @param height Maximum height
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder maxSize(int width, int height) {
         this.panel.setMaximumSize(new Dimension(width, height));
         return this;
     }
 
+    /**
+     * Sets the minimum size of the JPanel
+     * @param width Minimum width
+     * @param height Minimum height
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder minSize(int width, int height) {
         this.panel.setMinimumSize(new Dimension(width, height));
         return this;
     }
 
+    /**
+     * Sets the preferred size of the JPanel. This should be used instead of setSize()
+     * as that method does not work on JPanels with a LayoutManager, like BoxLayout here
+     * @param width Preferred width
+     * @param height Preferred height
+     * @return This BoxBuilder instance
+     */
     public BoxBuilder preferredSize(int width, int height) {
         this.panel.setPreferredSize(new Dimension(width, height));
         return this;
     }
 
+    /**
+     * Returns the JPanel with BoxLayout with all the specified properties
+     * @return The JPanel with BoxLayout with all the specified properties
+     */
     public JPanel build() {
         return this.panel;
     }
--- src/ButtonBuilder.java
+++ src/ButtonBuilder.java
@@ -1,28 +1,62 @@
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
+/**
+ * A class to allow building a JButton using the Builder Pattern
+ * @see <a href="https://en.wikipedia.org/wiki/Builder_pattern">Builder Pattern</a>
+ */
 public class ButtonBuilder {
     private JButton button = new JButton();
 
+    /**
+     * Constructs a new ButtonBuilder instance
+     */
     public ButtonBuilder() {}
 
+    /**
+     * Constucts a new ButtonBuilder instance and registers the JButton
+     * @param id The ID of the JButton
+     */
     public ButtonBuilder(String id) {Components.reg(id, this.button);}
 
+    /**
+     * Sets the text of the JButton
+     * @param text The desired text
+     * @return This ButtonBuilder instance
+     */
     public ButtonBuilder text(String text) {
         this.button.setText(text);
         return this;
     }
 
+    /**
+     * Sets whether the JButton is enabled
+     * @param enabled Whether the JButton should be enabled
+     * @return This BoxBuilder instance
+     */
     public ButtonBuilder enabled(boolean enabled) {
         this.button.setEnabled(enabled);
         return this;
     }
 
+    /**
+     * Sets the ActionListener of the JButton. For example, if you would like
+     * to handle events on the app level and have implemented the ActionListener 
+     * interface for the app, pass the app to this method. All events generated 
+     * by this JButton will then be forwarded to the app
+     * @param actionListener A class that implements the ActionListener interface
+     * and to which events from this JButton will be forwarded to
+     * @return This ButtonBuilder instance
+     */
     public ButtonBuilder actionListener(ActionListener actionListener) {
         this.button.addActionListener(actionListener);
         return this;
     }
 
+    /**
+     * Returns the JButton with all specified properties
+     * @return The JButton with all specified properties
+     */
     public JButton build() {
         return this.button;
     }
